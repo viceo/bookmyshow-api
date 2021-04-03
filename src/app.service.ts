407ab215ad309d70728e0392ed38771549575164
Leopoldo Muñoz <viceo00@gmail.com>
2021-04-03 14:50:36 -0600

    se implementa servicio para validar NODE_ENV

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,12 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
+
   getHello(): string {
     return 'Hello World!';
+  }
+
+  getEnv(): string {
+    return process.env.NODE_ENV || 'development'
   }
 }
