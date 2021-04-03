01d58e01cc1414069a5e260f3439accee85ae977
Leopoldo Muñoz <viceo00@gmail.com>
2021-04-03 14:42:07 -0600

    se implementa middleware de seguridad, denegamos tráfico sin token privado. Por pruebas se deja HARDCODED. Se recomienda cambiar el token antes de salir a producción. Se recomienda implementar seguridad mediante Security Groups de AWS.

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Request, Response, NextFunction } from 'express'
+
+const SecurityMiddleware = (req: Request, res: Response, next: NextFunction) => {
+  
+  //? Solución momentaria para denegar tráfico directo a Beanstalk. Se redirecciona tráfico desde API Gateway para control de
+  //? autorizadores y otros servicios AWS.
+  if(process.env.NODE_ENV === 'production') {
+    if (req.headers['x-api-key'] !== 'uMYNT2YfDJHEV9dd7R9ZkK5vnRA4wbLnKpuLprDBGF4R7m4MKVdfadzbR8un6cRvdrw4YrvzgeWHEpRvbNpfJpxYvLd7KaEsHfuJ9UWUgdnQFqLPbfSVTyn2xMhuhLDe') {
+      res.status(403).end()
+      return
+    }
+  }
+  next()
+}
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(SecurityMiddleware);
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
