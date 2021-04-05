f670a8d8564ca0c9b02231cdbd3db182da1be708
Leopoldo Muñoz <viceo00@gmail.com>
2021-04-04 19:39:03 -0500

    habilitando CORS

--- src/main.ts
+++ src/main.ts
@@ -18,6 +18,7 @@ const SecurityMiddleware = (req: Request, res: Response, next: NextFunction) => 
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors()
   app.use(SecurityMiddleware);
   await app.listen(process.env.PORT || 3000);
 }
