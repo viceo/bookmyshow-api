9fdaaab7933ecef81a70300bddf7d16ddf275bc0
Leopoldo Muñoz <viceo00@gmail.com>
2021-04-04 21:18:14 -0500

    seleccion distinct con query builder

--- src/cities/cities.service.ts
+++ src/cities/cities.service.ts
@@ -10,12 +10,20 @@ export class CitiesService {
         @InjectRepository(Show)
         private showsRepository: Repository<Show>
     ) { }
-    
+
     //? Por motivo de DEMO no se crea un catálogo de ciudades
     //? y se consultan las ciudades como fueron registradas en los shows
     async getCities() {
         //* Obtenemos ciudades de repositorio y las mapeamos a un array de strings
-        return (await this.showsRepository.find({ select: ['city'] })).map(x => x.city)
+        // return (await this.showsRepository.find({ select: ['city'] })).map(x => x.city)
+        const cities = await this.showsRepository
+            .createQueryBuilder('show')
+            .select('city')
+            .distinct(true)
+            .getRawMany()
+
+        return cities.map(x => x.city)
+            
     }
 
 }
